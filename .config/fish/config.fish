d09d5e488053cfeb1f5010ca0dbd642d23f86375
hiraki <hirakitaito@gmail.com>
2023-05-07 02:19:40 +0900

    change /usr/share/dotnet

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,4 @@ end
 set -x http_proxy http://172.29.112.1:3128
 set -x https_proxy http://172.29.112.1:3128
 set -x PATH $PATH:$HOME/.dotnet:$HOME/.dotnet/tools
-set -x DOTNET_ROOT $HOME/.dotnet
+set -x DOTNET_ROOT /usr/share/dotnet
