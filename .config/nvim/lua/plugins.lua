8e3615682aebe59223599fb16df13392a13dca23
hiraki <hirakitaito@gmail.com>
2023-05-06 17:33:42 +0900

    add and setup debugger

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -58,13 +58,13 @@ require("lazy").setup{
                 matchup = {
                     enable = true,
                 },
-                yati = {
-                    enable  = true,
-                    default_lazy = true,
-                    default_fallback = "auto",
-                },
+                --yati = {
+                --    enable  = true,
+                --    default_lazy = true,
+                --    default_fallback = "auto",
+                --},
                 indent = {
-                    enable = false,
+                    enable = true,
                 },
                 ensure_installed = {
                     "css","html", "javascript",
@@ -620,9 +620,94 @@ require("lazy").setup{
     },
     {
         "ntpeters/vim-better-whitespace",
-        event = "BufRead",
+        event = "InsertEnter",
         config = function()
-            vim.g.current_line_whitespace_disabled_hard=1
+            vim.g.better_whitespace_enabled=0
+        end,
+    },
+    {
+        "mfussenegger/nvim-dap",
+        ft = {
+            "cs",
+        },
+        config = function()
+            local dap = require("dap")
+            dap.adapters.coreclr = {
+                type = 'executable',
+                command = '/usr/local/bin/netcoredbg/netcoredbg',
+                args = {'--interpreter=vscode'}
+            }
+            dap.configurations.cs = {
+                {
+                    type = "coreclr",
+                    name = "launch - netcoredbg",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+                    end,
+                },
+            } 
+
+            vim.keymap.set('n', '<F5>', function() dap.continue() end)
+            vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+            vim.keymap.set('n', '<F11>', function() dap.step_into() end)
+            vim.keymap.set('n', '<F12>', function() dap.step_out() end)
+            vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
+            vim.keymap.set('n', '<Leader>B', function() dap.set_breakpoint() end)
+            vim.keymap.set('n', '<Leader>lp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+            vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
+            vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
+            vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+                require('dap.ui.widgets').hover()
+            end)
+            vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+                require('dap.ui.widgets').preview()
+            end)
+            vim.keymap.set('n', '<Leader>df', function()
+                local widgets = require('dap.ui.widgets')
+                widgets.centered_float(widgets.frames)
+            end)
+            vim.keymap.set('n', '<Leader>ds', function()
+                local widgets = require('dap.ui.widgets')
+                widgets.centered_float(widgets.scopes)
+            end)
+        end,
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        config = function()
+            local dap, dapui = require("dap"), require("dapui")
+            dapui.setup()
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
+        end,
+        dependencies = { "mfussenegger/nvim-dap" },
+        ft = { "cs" },
+    },
+    {
+        "theHamsta/nvim-dap-virtual-text",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        ft = { "cs" },
+    },
+    {
+        "folke/neodev.nvim",
+        config = function()
+            -- require("neodev").setup({
+                -- library = {
+                --     plugins = { "nvim-dap-ui" },
+                --     types = true,
+                -- },
+            -- })
         end,
     },
 }
