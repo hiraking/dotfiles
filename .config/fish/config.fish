f8298cb0a456e3f6dbe205f90de56c65f79e9126
hiraking <hirakitaito@gmail.com>
2024-01-10 23:40:29 +0900

    fishの設定を追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -x PATH $PATH:$HOME/.dotnet:$HOME/.dotnet/tools
+set -x PATH $PATH:$HOME/.local/bin
+set -x DOTNET_ROOT /usr/share/dotnet
+
+thefuck --alias | source
+
+# 会社支給のSurfaceでセキュリティソフトをすり抜けるためのプロキシサーバーの設定
 set -x http_proxy http://172.29.112.1:3128
 set -x https_proxy http://172.29.112.1:3128
-set -x PATH $PATH:$HOME/.dotnet:$HOME/.dotnet/tools
-set -x DOTNET_ROOT /usr/share/dotnet
+
