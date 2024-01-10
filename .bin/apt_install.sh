e3a2cebc437d6bab54fa5c613f10d05c05f3fed1
hiraking <63293446+hiraking@users.noreply.github.com>
2024-01-10 22:47:39 +0900

    オプションを追加

--- .bin/apt_install.sh
+++ .bin/apt_install.sh
@@ -1,13 +1,13 @@
 sudo apt update
 
-sudo apt install git
-sudo apt install python3-dev python3-pip python3-setuptools
-sudo apt install bat
-sudo apt install exa
-sudo apt install fzf
-sudo apt install ripgrep
-sudo apt install httpie
-sudo apt install fish
+sudo apt install -y git
+sudo apt install -y python3-dev python3-pip python3-setuptools
+sudo apt install -y bat
+sudo apt install -y exa
+sudo apt install -y fzf
+sudo apt install -y ripgrep
+sudo apt install -y httpie
+sudo apt install -y fish
 
-pip3 install thefuck
-pip3 install tldr
+pip3 install thefuck --user
+pip3 install tldr --user
