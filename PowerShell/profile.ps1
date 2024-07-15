fe13b7cbbbc923540f6a5e19574c6a6b89995e33
平木 大都 <hiraki@nextscape.net>
2024-07-15 19:32:44 +0900

    customize PowerShell prompt

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,9 +1,32 @@
 Import-Module -Name Terminal-Icons
 
-Import-Module -Name pure-pwsh
-# ANSI escape sequence "38;2;r;g;b"
-$pure.PwdColor    = "38;2;129;145;255"
-$pure.PromptColor = "38;2;61;216;255"
-$pure.ErrorColor  = "38;2;255;70;70"
+# ANSIエスケープシーケンスで色を指定
+$colors = @{
+  # "`e[38;2;{R};{G};{B}m"でRGBカラーを指定
+  Path = "`e[38;2;100;134;207m"
+  SymbolSuccess = "`e[38;2;107;255;191m"
+  SymbolError = "`e[38;2;219;75;91m"
+}
+
+# 色を適用するためのフィルタ
+filter fmtColor($color) { $_ ? "$color$_`e[0m" : '' }
+
+# プロンプト関数
+function prompt() {
+    $isError = !$?
+
+    # ドライブ名(C:) を除き、ホームディレクトリは ~ に変換する
+    $drive = $pwd.Drive.Name
+    $path = $pwd.path.Replace($HOME, "~").Replace("${drive}:", "")
+
+    Write-Host ""
+    Write-Host ($path | fmtColor $colors.Path)
+
+    $symbolColor = if ($isError) { $colors.SymbolError } else { $colors.SymbolSuccess }
+    Write-Host ("❯" | fmtColor $symbolColor) -NoNewLine
+
+    return " "
+}
+
 
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
