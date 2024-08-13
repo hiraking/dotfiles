7ce57b8ff799cf79c83d8dfc7bd851325a30e8bb
hiraking <hirakitaito@gmail.com>
2024-08-13 22:19:16 +0900

    fishの環境変数の設定方法を変更

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -x PATH $PATH:$HOME/.dotnet:$HOME/.dotnet/tools
-set -x PATH $PATH:$HOME/.local/bin
 set -x DOTNET_ROOT /usr/share/dotnet
 
 thefuck --alias | source
